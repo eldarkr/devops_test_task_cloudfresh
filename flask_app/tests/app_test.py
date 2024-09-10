9c18e812810d1a22da076208b6709c88075f9072
Eldar Kerimov <goreldar@gmail.com>
2024-09-10 22:36:33 +0300

    Update test

--- flask_app/tests/app_test.py
+++ flask_app/tests/app_test.py
@@ -5,4 +5,4 @@ def test_home():
     with app.test_client() as client:
         response = client.get('/')
         assert response.status_code == 200
-        assert response.data == b"App for test task"
+        assert response.data == b"Test task for Cloudfresh"
